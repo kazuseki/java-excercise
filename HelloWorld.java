35ca17bcdc18bb2bdf7303edd3cfaa3a600b1f86
kazuseki <47818409+kazuseki@users.noreply.github.com>
2019-08-22 18:13:32 +0900

    vscodeでどんな補完がされるか確認

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class Main{
+    
+    private static final String HELLO_WORLD = "Hello World";
+
+    public static void main(String[] args){
+        System.out.println(HELLO_WORLD);
+    }
+}
